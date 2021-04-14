a8a8b05bddc97cab2fc4f10540bf1c2f1ce9be65
cheollie <>
2021-04-14 02:55:31 +0000

    testing css again

--- timer.js
+++ timer.js
@@ -1,6 +1,17 @@
 timer = setInterval(increment, 1000, time);
-    
+  
+}
+const timer = document.getElementById('name');
 
+var hour = 0;
+var min = 0;
+var sec = 0;
+bool start = false;
+
+function startTimer() {
+  if (start == false) {
+    start
+  }
 }
 
 
