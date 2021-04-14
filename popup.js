da17883d102573fb4906c0fe7844bca15c531b38
Aaron Zhu <>
2021-04-14 11:23:15 +0000

    setup pomodoro timer

--- popup.js
+++ popup.js
@@ -1,9 +1,12 @@
 // runs everytime the pop up is clicked
-
-document.getElementById("PomodoroButton").onclick = () => setPomodoro();
+window.onload = function() {
+    document.getElementById("MainMenu").hidden = false;
+    document.getElementById("PomodoroButton").onclick = () => setPomodoro();
+}
 
 
 function setPomodoro() {
     document.getElementById("MainMenu").hidden = true;
-    
+    document.getElementById("SetupTimer").hidden = false;
+
 }
