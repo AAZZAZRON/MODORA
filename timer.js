3cca46980a1863668497f70a459d319be238bd2b
cheollie <>
2021-04-14 02:59:29 +0000

    testing css again

--- timer.js
+++ timer.js
@@ -10,10 +10,48 @@ bool start = false;
 
 function startTimer() {
   if (start == false) {
-    start
+    start = true;
+    timing();
   }
 }
+function stoptTimer() {
+  if (start == true) {
+    start = false;
+  }
+}
+function timing() {
+  if(start == true) {
+    sec = parseInt(sec);
+    min = parseInt(min);
+    hr = parseInt(hr);
 
+    sec += 1;
+
+    if (sec == 60) {
+      min += 1;
+      sec = 0;
+    }
+    if (min == 60) {
+      hr = hr + 1;
+      min = 0;
+      sec = 0;
+    }
+
+    if (sec < 10 || sec == 0) {
+      sec = '0' + sec;
+    }
+    if (min < 10 || min == 0) {
+      min = '0' + min;
+    }
+    if (hr < 10 || hr == 0) {
+      hr = '0' + hr;
+    }
+
+    timer.innerHTML = hr + ':' + min + ':' + sec;
+
+    setTimeout("timing()", 1000);
+  }
+}
 
 
 
