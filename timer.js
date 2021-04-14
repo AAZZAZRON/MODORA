6ee8fee21b7d92f7e441bd9f4330af134d24de24
cheollie <>
2021-04-14 18:55:45 +0000

    testing website list screen and other stuff too

--- timer.js
+++ timer.js
@@ -1,17 +1,24 @@
+var counter = 1;
 function createTimer() {
   document.getElementById("timer").innerText = "00:00:00";
+  document.getElementById("subtitle").innerText = "Pomodoro Cycle " + counter;
   chrome.runtime.sendMessage({message: "start stopwatch"})
 }
 
 function abortTimer() {
   chrome.runtime.sendMessage({message: "abort"})
+  cycle = 1;
 }
 
 chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
       console.log(request, sender, sendResponse);
       if (request.message == "done") {
-          // do the done stuff
+        if (cycle == 4) {
+
+        } else {
+          alert(`You have completed Cycle ${cycle} of the Pomodoro!\nPlease take a five minute break.`);
+        }
       } else {
         document.getElementById("timer").innerText = request.message;
       }
--- todo.js
+++ todo.js
@@ -1,39 +1,19 @@
 var arr = [];
 
-var btn = document.getElementById("ToDoListButton");
-btn.addEventListener("click", function() {
-	setToDoList();
-})
+function getToDoList() {
+  var s = getCookie("todolist");
+  arr = s.split("\n");
+}
 
-function setToDoList() {
-  document.getElementById("MainMenu").hidden = true;
-  document.getElementById("SetupTimer").hidden = true;
-  document.getElementById("TimerOn").hidden = true;
-  document.getElementById("ToDoList").hidden = false;
-  for(let i = 0; i < arr.length; i += 1) {
-    
+document.getElementById("addtask").onclick = () => display();
+
+function display(){
+	var n = document.getElementById("userName").value;
+  arr.push(n);
+  var p = "";
+  for(let i = 0; i < arr.length; i+=1) {
+    p += arr[i] + "\n";
   }
+  document.getElementById("printhere").innerHTML = p;
+  addCookie("todolist", p);
 }
-
-var btn2 = document.getElementById("todobutton");
-btn2.addEventListener("click", function() {
-	arr.push(document.getElementById("name").value);
-  deleteCookie("listtodo");
-  addCookie("listtodo", arr);
-  setToDoList();
-}) 
-/*
-max
-do something like this:
-
-chooseBlocked = document.createElement("div");
-chooseBlocked.id = "ChooseBlocked";
-document.getElementById("choose-blocked-form").appendChild(chooseBlocked);
-const things = decodeURIComponent(document.cookie).split("; ");
-for (let i = 0; i < things.length; i += 1) {
-    const values = things[i].split("===");
-    if (values[1] == "banned") {
-        addToBlockedList(values[0]); // add website to blocked list
-    }
-}
-    */
