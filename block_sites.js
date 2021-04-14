f1f61daf24872c18b5933ea231c85d29f0917690
Aaron Zhu <>
2021-04-14 23:05:20 +0000

    new files

--- block_sites.js
+++ block_sites.js
@@ -7,8 +7,8 @@ function checkUrl() {
   for(int i = 0; i < addToBlockedList.length(); i++) {
     if(url == addToBlockedList[i]) {
     alert("It is Work Time Right Now Get Off!");
-  }
+    }
   }
 }
-}
+
 
