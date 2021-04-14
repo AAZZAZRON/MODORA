3471cf7af97c5850b8a5a50aeecf96600149179d
cheollie <>
2021-04-14 12:06:33 +0000

    testing setup screen css

--- popup.js
+++ popup.js
@@ -6,7 +6,16 @@ window.onload = function() {
 
 
 function setPomodoro() {
+    //hide and unhide
     document.getElementById("MainMenu").hidden = true;
     document.getElementById("SetupTimer").hidden = false;
+    document.getElementById("SelectAll").onclick = () => toggle(document.getElementById("SelectAll").checked);
+}
 
+function toggle(source) { // check and uncheck all items
+    var checkboxes = document.getElementsByName("x");
+    console.log(checkboxes);
+    for (let i = 0; i < checkboxes.length; i += 1) {
+        checkboxes[i].checked = source;
+    }
 }
