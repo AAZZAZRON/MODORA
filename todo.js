ba7fdd81f5dd75f1e965a1bd1bf52d75c3dac549
cheollie <>
2021-04-14 19:06:36 +0000

    trying to fix abort screen

--- todo.js
+++ todo.js
@@ -2,18 +2,14 @@ var arr = [];
 
 function getToDoList() {
   var s = getCookie("todolist");
-  arr = s.split("\n");
+  arr = s.split("<br>");
 }
 
 document.getElementById("addtask").onclick = () => display();
 
 function display(){
-	var n = document.getElementById("userName").value;
-  arr.push(n);
-  var p = "";
-  for(let i = 0; i < arr.length; i+=1) {
-    p += arr[i] + "\n";
-  }
+	arr.push(document.getElementById("userName").value);
+  var p = arr.toString().replaceAll(",", "<br>");
   document.getElementById("printhere").innerHTML = p;
   addCookie("todolist", p);
 }
