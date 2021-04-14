0d991f1820d27d46518c7bc27cce6970c50a656d
cheollie <>
2021-04-14 20:01:27 +0000

    testeu websites screen css part 5 ihdk

--- block_sites.js
+++ block_sites.js
@@ -1,5 +1,7 @@
 // if site is bad site and we are in "lockdown", do not let site open
 function blockWebsite(urls) {
-    
+    if(chrome.webRequest.onBeforeRequest.hasListener(blockRequest))
+     chrome.webRequest.onBeforeRequest.removeListener(blockRequest);
+   chrome.webRequest.onBeforeRequest.addListener(blockRequest, {urls: ["*://*.{ $websiteName }/*", "*://*.facebook.net/*"]}, ['blocking']);
 }
 
