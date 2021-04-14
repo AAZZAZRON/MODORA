959408df9068124b90062c49cfab36e19abe36b9
cheollie <>
2021-04-14 14:45:49 +0000

    css - mainmenu+setup screen (23)

--- cookie_functions.js
+++ cookie_functions.js
@@ -11,15 +11,18 @@ function getCookie(name) {
             }
         }
         if (found) {
-            return things[i].split(find)[1];
+            return things[i].split(find)[1]; // returns value
         }
     }
-    return "notFound";
+    return "";
 }
 
 function addCookie(name, value) {
-    var expire = "expires=Fri, 31 Dec 3000 23:59:59 GMT";
-    document.cookie += `; ${name}=${value}; ${expire}`;
-    console.log(document.cookie);
-    console.log(decodeURIComponent(document.cookie));
+    var expire = "expires=Fri, 3 Jan 3000 23:59:59 GMT";
+    document.cookie = `${name}=${value}; ${expire};path=/`;
 }
+
+function deleteCookie(name) {
+    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`
+}
+
