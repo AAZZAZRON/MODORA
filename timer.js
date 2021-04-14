68ac7bab83c61ed9bcb5d60365c7adfcc7ca79e9
cheollie <>
2021-04-14 02:52:12 +0000

    testing css again

--- timer.js
+++ timer.js
@@ -1,23 +1,6 @@
-timer = setInterval(increment, 1000, timeNeeded);
+timer = setInterval(increment, 1000, time);
+    
 
-function timeToString(time) {
-  let diffInHrs = time / 3600000;
-  let hh = Math.floor(diffInHrs);
-
-  let diffInMin = (diffInHrs - hh) * 60;
-  let mm = Math.floor(diffInMin);
-
-  let diffInSec = (diffInMin - mm) * 60;
-  let ss = Math.floor(diffInSec);
-
-  let diffInMs = (diffInSec - ss) * 100;
-  let ms = Math.floor(diffInMs);
-
-  let formattedMM = mm.toString().padStart(2, "0");
-  let formattedSS = ss.toString().padStart(2, "0");
-  let formattedMS = ms.toString().padStart(2, "0");
-
-  return `${formattedMM}:${formattedSS}:${formattedMS}`;
 }
 
 
