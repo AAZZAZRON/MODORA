67fc50a86621ce708b8410d6522b6b122917bdb3
SamuelVJ <>
2021-04-15 03:15:49 +0000

    bug fixed

--- popup.js
+++ popup.js
@@ -216,32 +216,16 @@ function discardSites(instances) {
     return instances;
 }
 
-if(breakOrWork == true) {
   var urlss = "";
-  function checkUrl() {
+  function checkUrls() {
   urlss = window.location.host.toString();
   for(let i = 0; i < addToBlockedList.length(); i++) {
-    if(url == addToBlockedList[i] && ) {
+    if(url == addToBlockedList[i] && breakOrWork == true;) {
       alert("Stop Procrastinating! Close your tab!")
         window.close();
     }
   }
 }
-}
 
 
-// else {
-//   alert("Good Choice :D");
-// }
 
-// for(;;) {
-//   spammer();
-// }
-
-// function spammer() {
-//   for(let i = 0; i < 3; i++) {
-//     alert("It is Work Time Right Now Get Off!");
-//   }
-  
-//   alert("You have 10 seconds to get off this website!");
-// }
