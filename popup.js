5d7454a9d78df7896e4c08efe6d164db45ecf4eb
AAZZAZRON <aaronzhu3.1415@gmail.com>
2021-04-17 11:04:38 -0400

    tools.js

--- popup.js
+++ popup.js
@@ -54,7 +54,7 @@ function setPomodoro() { // show pomodoro setup screen
         mainMenu();
     };
     document.getElementById("start-button").onclick = () => {
-        sendBadSites(document.getElementById("ChooseBlocked").children); 
+        badArray = sendBadSites(document.getElementById("ChooseBlocked").children); 
         chooseBlocked.remove();
         timerSetup();
     }
@@ -69,19 +69,6 @@ function setPomodoro() { // show pomodoro setup screen
         addToBlockedList(things[i]); // add website to blocked list
     }   
 }
-
-function sendBadSites(instances) { // creates a global array of the "bad arrays", which is then used by the background to check the websites
-    badArray = ["null"];
-    var ind = instances.length - 2;
-    while (ind >= 0) {
-        if (instances[ind].checked) {
-            badArray.push(instances[ind + 1].innerText.substring(0, instances[ind + 1].innerText.length - 2));
-        }
-        ind -= 2;
-    }
-    chrome.runtime.sendMessage({message: "update badLinks", content: `${badArray.join(", ")}`});
-}
-
 
 function timerSetup() { // set up the timer screen, start and reset timer
     document.getElementById("MainMenu").hidden = true;
@@ -118,19 +105,6 @@ function showCompletedScreen() {
     badArray = [];
     chrome.runtime.sendMessage({message: "update badLinks", content: "null"});
     addCookie("tracker", "mainMenu");
-}
-
-
-function toggle() { // check and uncheck all items
-    var checkboxes = document.getElementsByName("x");
-    if (check) {
-        check = false;
-    } else {
-        check = true;
-    }
-    for (let i = 0; i < checkboxes.length; i += 1) {
-        checkboxes[i].checked = check;
-    }
 }
 
 function addToBlockedList(inner) { // add known blocked website to the blocked website list (in HTML)
---
+++ tools.js
@@ -0,0 +1,24 @@
+function sendBadSites(instances) { // creates a global array of the "bad arrays", which is then used by the background to check the websites
+    arr = ["null"];
+    var ind = instances.length - 2;
+    while (ind >= 0) {
+        if (instances[ind].checked) {
+            arr.push(instances[ind + 1].innerText.substring(0, instances[ind + 1].innerText.length - 2));
+        }
+        ind -= 2;
+    }
+    chrome.runtime.sendMessage({message: "update badLinks", content: `${arr.join(", ")}`});
+    return arr;
+}
+
+function toggle() { // check and uncheck all items
+    var checkboxes = document.getElementsByName("x");
+    if (check) {
+        check = false;
+    } else {
+        check = true;
+    }
+    for (let i = 0; i < checkboxes.length; i += 1) {
+        checkboxes[i].checked = check;
+    }
+}
