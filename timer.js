64f7f576dd695b320a6d41690163c4f2c2da652e
AAZZAZRON <aaronzhu3.1415@gmail.com>
2021-08-19 14:31:59 -0400

    xtras

--- timer.js
+++ timer.js
@@ -1,6 +1,6 @@
 function createTimer() {
   document.getElementById("timer").innerText = "00:00:00";
-  document.getElementById("subtitle").innerText = "Pomodoro Cycle 1";
+  document.getElementById("subtitle").innerText = "Pomodoro Round 1 Cycle 1";
   chrome.runtime.sendMessage({message: "start stopwatch"})
   addCookie("timer-on", "on");
 }
